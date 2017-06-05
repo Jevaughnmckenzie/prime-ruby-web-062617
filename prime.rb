b9f6da67ba3015d2893a09466dc33aa8736280d3
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-06-05 19:03:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+
+
+
+def prime?(num)
+
+  truthy_array = []
+
+  if ((num > 2) || (num < -2))
+    mid_num = (num)/ 2
+    test_range = 2..mid_num
+
+    test_range.to_a.each do | test_value |
+
+      (num % test_value != 0) ? truthy_array << true : truthy_array << false
+
+    end
+  else
+    return false
+  end
+
+  truthy_array.all? { |e| e == true }
+
+end
